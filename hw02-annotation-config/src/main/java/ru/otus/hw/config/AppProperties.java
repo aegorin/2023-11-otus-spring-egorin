ffd50491184b159d1282aae6d9de0b42b03a0f99
egorin.av <aleksey@egorin.ru>
2023-12-18 20:37:57 +0300

    Перенести @PropertySource в Application

--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -3,10 +3,8 @@ package ru.otus.hw.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @Data
 public class AppProperties implements TestFileNameProvider, TestConfig {
 
