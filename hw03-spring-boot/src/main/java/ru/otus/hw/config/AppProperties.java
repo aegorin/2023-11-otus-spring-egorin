c460378e347d70d37a73f7f59a8098de45f2fcc4
egorin.av <aleksey@egorin.ru>
2023-12-23 12:14:22 +0300

    Перенести свойства в application.yml

--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,20 +1,17 @@
 package ru.otus.hw.config;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Component
-@Data
+@ConfigurationProperties(prefix = "test")
 public class AppProperties implements TestFileNameProvider, TestConfig {
 
     private final String testFileName;
 
     private final int rightAnswersCountToPass;
 
-    public AppProperties(@Value("${test.fileName}") String testFileName,
-                         @Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass) {
-        this.testFileName = testFileName;
+    public AppProperties(String fileName,
+                         int rightAnswersCountToPass) {
+        this.testFileName = fileName;
         this.rightAnswersCountToPass = rightAnswersCountToPass;
     }
 
