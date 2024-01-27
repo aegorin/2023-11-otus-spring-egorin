3c991907ae7ef31591871a8086a32c37c6981ca7
egorin.av <aleksey@egorin.ru>
2024-01-27 13:03:12 +0300

    Переключить репозиторий авторов на JPA

--- hw06-jpql/src/test/java/ru/otus/hw/repositories/JpaAuthorRepositoryTest.java
+++ hw06-jpql/src/test/java/ru/otus/hw/repositories/JpaAuthorRepositoryTest.java
@@ -2,7 +2,8 @@ package ru.otus.hw.repositories;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import ru.otus.hw.models.Author;
 
@@ -10,19 +11,30 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@JdbcTest
-@Import(JdbcAuthorRepository.class)
-class JdbcAuthorRepositoryTest {
+@DataJpaTest
+@Import(JpaAuthorRepository.class)
+class JpaAuthorRepositoryTest {
     @Autowired
     private AuthorRepository authorRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     void returnAuthorById() {
-        assertThat(authorRepository.findById(1))
-                .isNotEmpty()
+        var actualAuthor = authorRepository.findById(1);
+        var expectedAuthor = em.find(Author.class, 1);
+        assertThat(actualAuthor)
+                .isPresent()
                 .get()
                 .usingRecursiveComparison()
-                .isEqualTo(new Author(1, "Author_1"));
+                .isEqualTo(expectedAuthor);
+    }
+
+    @Test
+    void returnEmptyWhenNotFound() {
+        assertThat(authorRepository.findById(-1))
+                .isEmpty();
     }
 
     @Test
