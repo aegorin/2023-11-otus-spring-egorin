7b241fbbadfead3b138c0d84debe6500401b423d
egorin.av <aleksey@egorin.ru>
2024-01-27 13:16:56 +0300

    Переключить репозиторий жанров на JPA

--- hw06-jpql/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
+++ hw06-jpql/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
@@ -2,7 +2,8 @@ package ru.otus.hw.repositories;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import ru.otus.hw.models.Genre;
 
@@ -10,19 +11,24 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@JdbcTest
-@Import(JdbcGenreRepository.class)
-class JdbcGenreRepositoryTest {
+@DataJpaTest
+@Import(JpaGenreRepository.class)
+class JpaGenreRepositoryTest {
     @Autowired
     private GenreRepository genreRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     void returnGenreById() {
-        assertThat(genreRepository.findById(1L))
-                .isNotEmpty()
+        var actualGenre = genreRepository.findById(1L);
+        var expectedGenre = em.find(Genre.class, 1);
+        assertThat(actualGenre)
+                .isPresent()
                 .get()
                 .usingRecursiveComparison()
-                .isEqualTo(new Genre(1L, "Genre_1"));
+                .isEqualTo(expectedGenre);
     }
 
     @Test
