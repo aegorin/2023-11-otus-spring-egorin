1433720f8c94dfa1263f4ff79fae8cc25a3cc00c
egorin.av <aleksey@egorin.ru>
2024-02-01 23:56:06 +0300

    Указать запрос для получения комментариев по id книги
    Теперь запрос будет выполняться без join к таблице книг.
    Книга, для которой выбираются комментарии, уже может
    находиться в PersistentContext. Не нужно получать
    избыточные данные от СУБД.

--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.hw.models.Comment;
 
@@ -7,5 +8,6 @@ import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 
+    @Query("SELECT c FROM #{#entityName} c WHERE c.book.id = :bookId")
     List<Comment> findByBookId(Long bookId);
 }
