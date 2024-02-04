19096ca7eb4434af96af7615704547c934ca8564
egorin.av <aleksey@egorin.ru>
2024-02-04 16:16:36 +0300

    Запрос @Query здесь можно не указывать

--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.hw.models.Comment;
 
@@ -8,6 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 
-    @Query("SELECT c FROM #{#entityName} c WHERE c.book.id = :bookId")
     List<Comment> findByBookId(Long bookId);
 }
