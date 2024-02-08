634d8776ed6c76dec352bff6fc9add63bc4324f3
egorin.av <aleksey@egorin.ru>
2024-02-08 15:21:39 +0300

    Убрать зависимость Spring Shell

--- hw09-mvc/src/main/java/ru/otus/hw/Application.java
+++ hw09-mvc/src/main/java/ru/otus/hw/Application.java
@@ -2,13 +2,12 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
-@CommandScan("ru.otus.hw.shell")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("\n\nOpen in your browser http://localhost:8080/");
     }
 }
